3bc92906a8e7588531ca68a9efd999fd5420904a
cristobalfuenzalida <crfuenza@dcc.uchile.cl>
2023-02-20 02:31:24 -0300

    Change driver path

--- scraper_bot.py
+++ scraper_bot.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.headless = True
-driver = webdriver.Chrome("/usr/bin/chromedriver", options=options)
+driver = webdriver.Chrome("/usr/local/bin/chromedriver", options=options)
 driver.get("https://google.com/")
 print(driver.title)
 driver.quit()
