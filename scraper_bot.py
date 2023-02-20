0107d56deff2c17b4932eb7111a509d5c823f0be
cristobalfuenzalida <crfuenza@dcc.uchile.cl>
2023-02-20 02:50:52 -0300

    Fix driver path

--- scraper_bot.py
+++ scraper_bot.py
@@ -2,8 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
-options.headless = True
-driver = webdriver.Chrome("/usr/local/bin/chromedriver", options=options)
-driver.get("https://google.com/")
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+driver = webdriver.Chrome('/usr/local/bin/chromedriver', options=options)
+driver.get('https://google.com/')
 print(driver.title)
 driver.quit()
