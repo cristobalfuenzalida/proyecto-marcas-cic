075feb2d1f91afe9781e4ed67b66cc156c344f08
cristobalfuenzalida <crfuenza@dcc.uchile.cl>
2023-02-20 03:00:40 -0300

    Fix driver access with service class

--- scraper_bot.py
+++ scraper_bot.py
@@ -1,10 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
-driver = webdriver.Chrome('/usr/local/bin/chromedriver', options=options)
+
+driver = webdriver.Chrome(
+    service=Service('/usr/local/bin/chromedriver'),
+    options=options
+)
+
 driver.get('https://google.com/')
 print(driver.title)
 driver.quit()
